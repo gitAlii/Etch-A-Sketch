17fd1d8e6154c093fe804538232e0e0f5dbdee18
Mahbub Ali <Mahbubali02@outlook.com>
2023-05-22 13:27:28 +0100

    adding 16x16 grid

---
+++ index.js
@@ -0,0 +1,29 @@
+//create a main div element
+const mainDiv = document.createElement('div');
+
+//Gives classname of maindiv
+mainDiv.className = 'mainDiv';
+
+
+
+//puts a div classname-sketchBoard into the main div x16
+for (i = 0; i < 16; i++) {
+    //global scope
+    let sketchBoard = document.createElement('div');
+    sketchBoard.className = 'sketchBoard';
+    
+    for(j = 0; j <16; j++) {
+        //function block scope
+        let smallSquare = document.createElement('div');
+        smallSquare.className = 'smallSquare';
+        sketchBoard.append(smallSquare)
+
+    }
+    
+    //slects the maindiv class and adds sketchBoard div to it as a child element
+    mainDiv.append(sketchBoard);
+    
+};
+
+//append the main div to the body to display
+document.body.append(mainDiv);
